68734149eab13dac7d6c6f5bcd4c3339cef44edd
Josef Sartori <josef.sartori@outlook.com>
2022-07-26 09:32:14 -0300

    atualização de carousel

--- js/script.js
+++ js/script.js
@@ -3,25 +3,26 @@ let blog = document.getElementById("link2");
 let sobre = document.getElementById("link3");
 
 contato.addEventListener("mouseover", function() {
-    contato.classList.add("active");    
+    contato.classList.add("activa");    
 });
 
 contato.addEventListener("mouseleave", function() {
-    contato.classList.remove("active");
+    contato.classList.remove("activa");
 });
 
 blog.addEventListener("mouseover", function() {
-    blog.classList.add("active");
+    blog.classList.add("activa");
 });
 
 blog.addEventListener("mouseleave", function() {
-    blog.classList.remove("active");
+    blog.classList.remove("activa");
 });
 
 sobre.addEventListener("mouseover", function() {
-    sobre.classList.add("active");
+    sobre.classList.add("activa");
 });
 
 sobre.addEventListener("mouseleave", function() {
-    sobre.classList.remove("active");
+    sobre.classList.remove("activa");
 });
+
