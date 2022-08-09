2653d889cbfe15cb6ede97453b0be39bf58a3150
JosefStonon <josef.sartori@outlook.com>
2022-08-09 16:34:18 -0300

    atl

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+var NOME = lines.shift();
+var SALARIO = lines.shift();
+var VENDAS = lines.shift();
+
+var LUCRO = VENDAS * 0.15;
+
+var TOTAL = VENDAS + LUCRO;
+
+
+
+console.log("TOTAL = R$ " + TOTAL);
